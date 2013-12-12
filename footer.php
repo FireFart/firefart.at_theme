50069744413575df72da92328cbe5c6234e4e0d0
Christian Mehlmauer <FireFart@gmail.com>
2013-12-12 21:50:54 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,28 +5,33 @@
  * Contains footer content and the closing of the #main and #page div elements.
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-		<?php
-			$start_year = 2013;
-			$current_year = date('Y');
-			if ($start_year == $current_year)
-			{
-				$text = $start_year;
-			}
-			else
-			{
-				$text = $start_year . '-' . $current_year;
-			}
-		?>
-			Copyright <?php echo $text; ?> by Christian Mehlmauer
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+
+				</div><!-- #main -->
+
+				<footer id="colophon" class="site-footer" role="contentinfo">
+
+						<?php get_sidebar( 'footer' ); ?>
+
+						<div class="site-info">
+							<?php
+								$start_year = 2013;
+								$current_year = date('Y');
+								if ($start_year == $current_year)
+								{
+									$text = $start_year;
+								}
+								else
+								{
+									$text = $start_year . '-' . $current_year;
+								}
+							?>
+							Copyright <?php echo $text; ?> by Christian Mehlmauer
+						</div><!-- .site-info -->
+				</footer><!-- #colophon -->
+		</div><!-- #page -->
 </body>
 </html>
