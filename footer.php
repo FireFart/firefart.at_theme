f5559ede98ee07a8ddc73f394a422aea2f63df91
Christian Mehlmauer <FireFart@gmail.com>
2014-03-22 00:04:51 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -41,14 +41,14 @@
   _paq.push(['enableLinkTracking']);
   (function() {
     var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.firefart.net/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setTrackerUrl', u+'pi.php']);
     _paq.push(['setSiteId', 1]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.defer=true; g.async=true; g.src=u+'pi.js'; s.parentNode.insertBefore(g,s);
   })();
 
 </script>
-<noscript><p><img src="http://www.firefart.net/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="http://www.firefart.net/pi.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 </body>
 </html>
